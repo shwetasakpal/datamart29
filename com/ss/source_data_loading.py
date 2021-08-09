ff2c8cf8a837b3efc1bb3bed0fee340803ed1aa7
Shweta <shwetasakpal1993@gmail.com>
2021-08-09 12:37:19 +0530

    test

---
+++ com/ss/source_data_loading.py
@@ -0,0 +1,108 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import*
+import yaml
+import os.path
+from com.ss.utils import aws_utils as ut
+
+
+if __name__ == '__main__':
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    # create the spark object
+    spark = SparkSession \
+        .builder \
+        .appName("Data Ingestion from Project Sources") \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_conf"]["uri"] + "/customer.address") \
+        .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.11:2.2.2') \
+        .getOrCreate()
+    # to log only the error logs in the console
+    spark.sparkContext.setLogLevel('ERROR')
+
+    # Reading Data from S3
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+    src_list = app_conf["source_list"]
+    for src in src_list:
+        src_conf = app_conf[src]
+        src_path = "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + src
+        if src == 'SB':
+            txn_df = ut.read_from_my_sql(spark,
+                                         src_conf["mysql_conf"]["dbtable"],
+                                         src_conf["mysql_conf"]["partition_column"],
+                                         app_secret
+                                         )
+            txn_df = txn_df.withColumn("ins_dt", current_date())
+            txn_df.show(5, False)
+            # write data to S3 in parquet format
+            txn_df \
+                .write \
+                .mode("overwrite") \
+                .partitionBy("ins_dt") \
+                .parquet(src_path)
+
+        elif src == 'OL':
+            # Reading Data from SFTP server
+            txn_df = ut.read_from_sftp(spark,
+                                       app_secret,
+                                       os.path.abspath(current_dir + "/../../" + app_secret["sftp_conf"]["pem"]),
+                                       src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv")
+            txn_df = txn_df.withColumn("ins_dt", current_date())
+            txn_df.show(5, False)
+            # write data to S3 in parquet format
+            txn_df \
+                .write \
+                .mode("overwrite") \
+                .partitionBy("ins_dt") \
+                .parquet(src_path)
+
+        elif src == 'CP':
+            txn_df = ut.read_from_s3(spark,
+                                     "s3a://" + app_conf["s3_conf"]["s3_bucket"] + src_conf["filename"])
+            cust_df = txn_df.withColumn("ins_dt", current_date())
+            cust_df.show(5)
+            # write data to S3
+            cust_df \
+                .write \
+                .mode("overwrite") \
+                .partitionBy("ins_dt") \
+                .parquet(src_path)
+
+        elif src == 'ADDR':
+            # Reading from mongodb
+            txn_df = ut.read_from_mongoDB(spark,
+                                          src_conf["mongodb_config"]["database"],
+                                          src_conf["mongodb_config"]["collection"])
+            txn_df = txn_df.withColumn("ins_dt", current_date())
+            txn_df.show(5)
+
+            txn_df = txn_df.withColumn("street", col("address.street")) \
+                .withColumn("City", col("address.city")) \
+                .withColumn("state", col("address.state")) \
+                .drop('address') \
+                .drop('_id')
+
+            txn_df.show(5)
+            # write data to S3
+            txn_df \
+                .write \
+                .mode("overwrite") \
+                .partitionBy("ins_dt") \
+                .parquet(src_path)
+
+# spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.apache.hadoop:hadoop-aws:2.7.4" com/ss/source_data_loading.py
+
+# spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.2.2,org.apache.hadoop:hadoop-aws:2.7.4" com/ss/source_data_loading.py
+
+
+
+
+
+
---
+++ com/ss/utils/aws_utils.py
@@ -0,0 +1,86 @@
+from json import load
+
+
+def get_jdbc_redshift_url(redshift_config:dict):
+    host = redshift_config["redshift_conf"]["host"]
+    port = redshift_config["redshift_conf"]["port"]
+    database = redshift_config["redshift_conf"]["database"]
+    username = redshift_config["redshift_conf"]["username"]
+    password = redshift_config["redshift_conf"]["password"]
+
+    return "jdbc:redshift://{}:{}/{}?user={}&password={}".format(host,port,database,username,password)
+
+def get_mysql_jdbc_url(mysql_config:dict):
+    host = mysql_config["mysql_conf"]["hostname"]
+    port = mysql_config["mysql_conf"]["port"]
+    database = mysql_config["mysql_conf"]["database"]
+
+    return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host,port,database)
+
+def read_from_my_sql(spark, table_name, part_col, secret_conf):
+    print("\n Reading from sql Database")
+    jdbc_params= {"url":get_mysql_jdbc_url(secret_conf),
+                "lowerBound": "1",
+                "UpperBound":"100",
+                "dbtable":table_name,
+                "partitionColumn": part_col,
+                "user": secret_conf["mysql_conf"]["username"],
+                "password": secret_conf["mysql_conf"]["password"]
+                 }
+    df =spark\
+        .read\
+        .format("jdbc")\
+        .option("driver","com.mysql.cj.jdbc.Driver")\
+        .option(**jdbc_params)\
+        .load()
+
+    return df
+def read_from_sftp(spark, app_secret, secret_file, filepath):
+    return spark.read \
+        .format("com.springml.spark.sftp")\
+        .option("host", app_secret["sftp_conf"]["hostname"])\
+        .option("port", app_secret["sftp_conf"]["port"])\
+        .option("username", app_secret["sftp_conf"]["username"])\
+        .option("pem", secret_file)\
+        .option("fileType", "csv")\
+        .option("delimiter", "|")\
+        .load(filepath)
+
+
+
+def read_from_s3(spark, path, delimiter='|', header='true'):
+    print("\nReading data from S3,")
+    df = spark.read \
+        .option("header", header) \
+        .format("csv") \
+        .option("delimiter", delimiter) \
+        .load(path)
+    return df
+
+
+def read_from_mongoDB(spark, database,collection):
+    print("\nReading data from MongoDB,")
+    df = spark.read \
+        .format("com.mongodb.spark.sql.DefaultSource") \
+        .option("database", database) \
+        .option("collection", collection) \
+        .load()
+    return df
+
+def read_parquet_from_s3(spark,file_path):
+    return spark.read \
+        .option("header", "true") \
+        .option("delimiter", "|") \
+        .parquet(file_path)
+
+
+def write_data_to_Redshift(txn_df,jdbc_url,s3_path,redshift_table_name):
+    txn_df.coalesce(1).write \
+            .format("io.github.spark_redshift_community.spark.redshift") \
+            .option("url", jdbc_url) \
+            .option("tempdir", s3_path) \
+            .option("forward_spark_s3_credentials", "true") \
+            .option("dbtable", redshift_table_name) \
+            .mode("overwrite") \
+            .save()
+
